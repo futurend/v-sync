3006d5cce5c047c17973a38b4f67ef7fe5cb1ab7
Claudio Rego <claudiojmrego@gmail.com>
2013-05-22 19:26:24 +0100

    dev

--- vidcomm.js
+++ vidcomm.js
@@ -212,7 +212,7 @@ process.on('SIGINT', function () {
 
 // check which player is available on the system
 require('child_process').exec('which omxplayer', function (error, stdout, stderr) {
-    console.log(stdout);
+    console.log(stdout[0] === '/');
     // if (stdout[0] !== '/') {
     //     require('child_process').exec('which mplayer', function (error, stdout, stderr) {
     //         if (stdout[0] !== '/') {
